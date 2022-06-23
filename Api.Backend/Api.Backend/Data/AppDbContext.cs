e87b36032c6206f9a1fd76f7d946a1b892b2753e
wagnergarnizet <61944236+wagnergarnizet@users.noreply.github.com>
2022-06-22 21:41:29 -0300

    Dbcontext

---
+++ Api.Backend/Api.Backend/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+
+        }
+        public DbSet<Campanha> Campanhas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Estoque> Estoques { get; set; }
+        public DbSet<Instituicao> Instituicaos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
