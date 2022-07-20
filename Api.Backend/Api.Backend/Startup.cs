2c5348734216a3e538afc958ad8fca36bee2b344
wagnergarnizet <61944236+wagnergarnizet@users.noreply.github.com>
2022-07-19 22:41:49 -0300

    docker

--- Api.Backend/Api.Backend/Startup.cs
+++ Api.Backend/Api.Backend/Startup.cs
@@ -39,6 +39,13 @@ namespace Api.Backend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+                context.Database.Migrate();
+            }
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
